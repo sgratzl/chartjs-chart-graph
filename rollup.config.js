ab778f94a488c498b9cf1f965dfce2afd6c254aa
Samuel Gratzl <sam@sgratzl.com>
2020-05-18 17:13:36 +0200

    update build config

--- rollup.config.js
+++ rollup.config.js
@@ -3,32 +3,29 @@ import pnp from 'rollup-plugin-pnp-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
+import pkg from './package.json';
 
 export default [
   {
     input: 'src/bundle.js',
     output: {
-      file: 'build/Chart.Graphs.js',
+      file: pkg.main,
       name: 'ChartGraphs',
       format: 'umd',
       globals: {
         'chart.js': 'Chart',
       },
     },
-    external: ['chart.js'],
+    external: Object.keys(pkg.peerDependencies),
     plugins: [commonjs(), pnp(), resolve(), babel({ babelHelpers: 'runtime' })],
   },
   {
     input: 'src/index.js',
     output: {
-      file: 'build/Chart.Graphs.esm.js',
-      name: 'ChartGraphs',
+      file: pkg.module,
       format: 'esm',
-      globals: {
-        'chart.js': 'Chart',
-      },
     },
-    external: ['chart.js', '@babel/runtime', 'd3-force', 'd3-hierarchy'],
+    external: Object.keys(pkg.peerDependencies).concat(Object.keys(pkg.dependencies)),
     plugins: [commonjs(), pnp(), resolve()],
   },
 ];
